8ffbc9ff324976e3507d5a098244a3cc7e65ae58
Pier Stoyanov <pier.stoyanov@gmail.com>
2023-06-22 17:13:53 +0300

    add dotenv load for docker; add docstring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import gc
 import os
 
+from dotenv import load_dotenv
 from flask import Flask, request, Response
 
 from bot import bot
@@ -11,6 +12,10 @@ from data_collection.functional.act_requests import data_management_with_request
 from data_collection.template_pattern.act_requests_template_patten import DataManagementWithRequests
 from logger.logger import logging
 from request_handlers import viber_request_handler
+
+# load dotenv vars if running inside docker image
+if os.getenv("RUNINDOCKER"):
+    load_dotenv()
 
 # Logger
 app_logger = logging.getLogger(__name__)
@@ -24,6 +29,7 @@ viber = bot.viber
 
 @app.route('/', methods=['POST'])
 def incoming():
+    """ Function to handle incomming requests from viber api."""
     app_logger.debug("received request. %s", request.get_data())
 
     if not viber.verify_signature(
@@ -46,7 +52,7 @@ def get_data():
 
     # selector variable that chooses between
     # act_requests and act_requests_template_pattern
-    use_requests = True
+    use_requests = False
 
     # if request.headers.get(gc_scheduler) == job_name:
     if use_requests:

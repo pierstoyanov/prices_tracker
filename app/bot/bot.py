71c002bc50542aade44d2b86ecf49bb2561b848a
Pier Stoyanov <pier.stoyanov@gmail.com>
2024-05-14 17:26:05 +0300

    add MessageManager class

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -2,6 +2,7 @@ import os
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
 from bot.messages import static_messages
+from bot.messages.message_manager import MessageManager
 from bot.users.composite_user_actions import CompositeUserActions
 from bot.messages.daily_data import get_daily_data_gsheets, build_daily_info
 from logger.logger import logging
@@ -23,9 +24,13 @@ class Bot:
         self.users = CompositeUserActions(storage_strategy)
         self.sheets_service = storage_manager.get_sheets_service()
         self.messages = static_messages
+        self.message_manager = MessageManager(sheets_service=self.sheets_service)
 
-    def daily_data(self, *args, **kwargs):
+    def get_daily(self, *args, **kwargs):
         return get_daily_data_gsheets(self.sheets_service, *args, **kwargs)
+
+    def get_last_data(self, *args, **kwargs):
+        return self.message_manager.get_daily_data_gsheets(*args, **kwargs)
 
     def build_daily_info(self):
         return build_daily_info(self.sheets_service)

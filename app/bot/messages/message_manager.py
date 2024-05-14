aad1c11b19708d4568ba1cadf30306ea11191d94
Pier Stoyanov <pier.stoyanov@gmail.com>
2024-05-14 17:25:47 +0300

    edit methods, add symbols dict

--- app/bot/messages/message_manager.py
+++ app/bot/messages/message_manager.py
@@ -1,12 +1,15 @@
 
+from datetime import datetime
+import os
+from bot.messages.symbols import symbols
 from logger.logger import logging
 from google_sheets.google_sheets_api_operations import get_multiple_named_ranges
 from firebase_admin import db
 
 class MessageManager():
-    def __init__(self, storage_service=0, sheets_service=None):
+    def __init__(self, storage=0, sheets_service=None):
         self.messages_logger = logging.getLogger('messages')
-        self.storage_service = storage_service
+        self.storage_service = storage
         self.sheets_service = sheets_service
         self.data_metals = db.reference('data/metals')
         self.data_power = db.reference('data/power')
@@ -37,6 +40,12 @@ class MessageManager():
             return
 
         if return_dict:
+            #TODO convert to unified data format
+            data = {"m": None, "r": None, "p": None}
+
+            
+
+
             return dict(zip(ranges,
                             [dict(zip(x['values'][0], x['values'][1])) for x in result]))
         return result
@@ -60,18 +69,36 @@ class MessageManager():
 
         return key_metals, data_metals, key_power, data_power, key_rates, data_rates
 
-
     
-    def get_data(self):
+    def build_daily_info_firebase(self):
         pass
 
+    def populate_text(self, data):
+        d = data
+        s = symbols
+        return f'' \
+        f'{s["calendar"]} Дата: {d["m"]["Date"]}\n' \
+        f'{s["chart"]} Мед\n' \
+        f'Offer: *{d["m"]["Offer"]:,.2f}{s["dollar"]}*\n' \
+        f'3 month: *{d["m"]["3mo"]:,.2f}{s["dollar"]}*\n' \
+        f'Stock: *{d["m"]["Stock"]:}*\n' \
+        f'{s["chart"]} Злато*\n' \
+        f'AM: *{d["m"]["Gold AM"]:,.3F}{s["dollar"]}*\n' \
+        f'PM *{d["m"]["Gold PM"]:,.3F}{s["dollar"]}*\n' \
+        f'Average: *{d["m"]["Average"]:.3F}{s["dollar"]}*\n' \
+        f'{s["chart"]} Сребро *{d["m"]["Silver"]:.4F}{s["dollar"]}*\n' \
+        f'{s["usd"]} BGN/USD: *{d["r"]["USD"]}*\n' \
+        f'{s["pound"]} BGN/GBP: *{d["r"]["GBP"]}*\n' \
+        f'{s["usd"]} BGN/CHF: *{d["r"]["CHF"]}*\n' \
+        f'{s["hv"]} Ел. енергия: {d["p"]["Date"]}\n' \
+        f'BGN: *{d["p"]["BGN"]:.2F}*\n' \
+        f'EUR: *{d["p"]["EUR"]:.2F}*\n' \
+        f'Volume *{d["p"]["Volume"]:.2F}*'
 
-    def build_daily_info(self, service):
+    def build_daily_info_gsheets(self):
         try:
-            c, cw, au, ag, rates, power = [dict(zip(x['values'][0], x['values'][1])) for x in get_daily_data_gsheets(service)]
-            date_status = test_date(c, cw, au, ag)
-            s_chart, s_dollar, s_calendar, s_usd, s_pound, s_hv \
-                = '\U0001F4C8', '\U0001F4B2', '\U0001F4C5', '\U0001F4B5', '\U0001F4B7', '\U000026A1'
+            c, cw, au, ag, rates, power = [dict(zip(x['values'][0], x['values'][1])) for x in self.get_daily_data_gsheets()]
+            date_status = self.test_date(c, cw, au, ag)
 
             # determine newer date
             lm_date, wm_date = datetime.strptime(c["Date"], "%d.%m.%Y"), \
@@ -84,21 +111,22 @@ class MessageManager():
                 c["Stock"] = cw["Stock"]
                 symbol = "Westmetal"
 
+            s = symbols
             text = f'' \
-                f'{s_calendar} Дата: {c["Date"]}\n {date_status}\n' \
-                f'{s_chart}Мед {symbol}\n' \
-                f'Offer: *{c["Offer"]:,.2f}{s_dollar}*\n' \
-                f'3 month: *{c["3mo"]:,.2f}{s_dollar}*\n' \
+                f'{s["calendar"]} Дата: {c["Date"]}\n {date_status}\n' \
+                f'{s["chart"]}Мед {symbol}\n' \
+                f'Offer: *{c["Offer"]:,.2f}*{s["dollar"]}\n' \
+                f'3 month: *{c["3mo"]:,.2f}*{s["dollar"]}\n' \
                 f'Stock: *{c["Stock"]:}*\n' \
-                f'{s_chart} Злато\n' \
-                f'AM: *{au["Gold AM"]:,.3F}{s_dollar}*\n' \
-                f'PM *{au["Gold PM"]:,.3F}{s_dollar}*\n' \
-                f'Average: *{au["Average"]:.3F}{s_dollar}*\n' \
-                f'{s_chart} Сребро *{ag["Silver"]:.4F}{s_dollar}*\n' \
-                f'{s_usd} BGN/USD: *{rates["USD"]}*\n' \
-                f'{s_pound} BGN/GBP: *{rates["GBP"]}*\n' \
-                f'{s_usd} BGN/CHF: *{rates["CHF"]}*\n' \
-                f'{s_hv} Ел. енергия: {power["Date"]}\n' \
+                f'{s["chart"]} Злато\n' \
+                f'AM: *{au["Gold AM"]:,.3F}{s["dollar"]}*\n' \
+                f'PM *{au["Gold PM"]:,.3F}{s["dollar"]}*\n' \
+                f'Average: *{au["Average"]:.3F}{s["dollar"]}*\n' \
+                f'{s["chart"]} Сребро *{ag["Silver"]:.4F}{s["dollar"]}*\n' \
+                f'{s["usd"]} BGN/USD: *{rates["USD"]}*\n' \
+                f'{s["pound"]} BGN/GBP: *{rates["GBP"]}*\n' \
+                f'{s["usd"]} BGN/CHF: *{rates["CHF"]}*\n' \
+                f'{s["hv"]} Ел. енергия: {power["Date"]}\n' \
                 f'BGN: *{power["BGN"]:.2F}*\n' \
                 f'EUR: *{power["EUR"]:.2F}*\n' \
                 f'Volume *{power["Volume"]:.2F}*'
---
+++ app/bot/messages/symbols.py
@@ -0,0 +1,8 @@
+symbols = {
+    "chart": '\U0001F4C8',
+    "dollar": '\U0001F4B2',
+    "calendar": '\U0001F4C5',
+    "usd": '\U0001F4B5',
+    "pound": '\U0001F4B7',
+    "hv": '\U000026A1'
+}

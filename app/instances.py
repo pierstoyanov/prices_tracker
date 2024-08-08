39d07a43d5baa86c1707c00a38983b5104bd5040
Pier Stoyanov <pier.stoyanov@gmail.com>
2024-08-08 13:33:32 +0300

    upd storage mngr & instances

--- app/instances.py
+++ app/instances.py
@@ -1,13 +1,18 @@
 from bot.bot import Bot
+from data_unit import DataUnit
 from storage.storage_manager import storage_manager
 
 
 # start firebase
-storage_manager.firebase()
+storage_manager.start_firebase()
 
-# Storage [firebase, gsheets, both]
-storage_strategy = "both"
+# last
+last_data = DataUnit().fill_data_from_gsheets()
 
 # viber bot Singleton init
-bot = Bot(storage_strategy)
+bot = Bot(storage_manager.get_storage_strategy(),
+          storage_manager.get_sheets_service())
+
 viber = bot.viber
+
+bot.message_manager.populate_daily_data()
--- app/storage/storage_manager.py
+++ app/storage/storage_manager.py
@@ -1,21 +1,34 @@
 from firebase_rt_db.firebase_serivce import start_firebase
 from google_sheets.google_service import GoogleService
 
+class StorageManagerSingleton:
+    """ Singleton for providing storage connection services. """   
+    def __init__(self, storage_strategy=0):
+        """ Storage strategy:
+            0 - firebase
+            1 - gsheets
+            2 - both (default)"""
 
-class StorageManager:
-    
-    def __init__(self):
         # google api service instance for using sheets
         # self.service = GoogleService.build_default_google_service()
         self.service = GoogleService.build_google_service("./service-account.json")
+        # Storage [firebase, gsheets, both]
+        self.storage_strategy = storage_strategy
 
-    @staticmethod
-    def firebase(): # firebase
-        start_firebase()
-    
+    def get_storage_strategy(self):
+        """ Storage strategy:
+            0 - firebase
+            1 - gsheets
+            2 - both"""
+        return self.storage_strategy
+
     def get_sheets_service(self):
         return self.service
+    
+    @staticmethod
+    def start_firebase(): # firebase
+        start_firebase()
 
 
 # Storage manger instance
-storage_manager = StorageManager()
+storage_manager = StorageManagerSingleton(2)

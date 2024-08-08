7ff23ee5f148d8754236c9636c3ce8f0db866662
Pier Stoyanov <pier.stoyanov@gmail.com>
2024-08-08 13:50:36 +0300

    upd symbols

--- app/bot/messages/symbols.py
+++ app/bot/messages/symbols.py
@@ -2,7 +2,11 @@ symbols = {
     "chart": '\U0001F4C8',
     "dollar": '\U0001F4B2',
     "calendar": '\U0001F4C5',
-    "usd": '\U0001F4B5',
-    "pound": '\U0001F4B7',
-    "hv": '\U000026A1'
+    "USD": '\U0001F4B5',
+    "GBP": '\U0001F4B7',
+    "highVolt": '\U000026A1',
+    "checkMark": '\U00002714',
+    "leftArrow": '\U00002B05',
 }
+
+c, chk_mrk, left_arr = '\U000000A9', '\U00002714', '\U00002B05'

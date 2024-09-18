780416e75386da891347588c6b06c8b7445ed511
Pier Stoyanov <pier.stoyanov@gmail.com>
2024-09-18 12:54:28 +0300

    upd instances

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -1,11 +1,12 @@
+from email import message
 import os
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
 from bot.messages import static_messages
 from bot.messages.message_manager import MessageManager
 from bot.users.composite_user_actions import CompositeUserActions
-from bot.messages.daily_data import get_daily_data_gsheets, build_daily_info
 from logger.logger import logging
+from storage.storage_manager import storage_manager as sm
 
 
 # begin bot class
@@ -20,20 +21,23 @@ class Bot:
     def __init__(self, storage_strategy: int, sheets_service):
         self.viber = Api(self.bot_configuration)
         self.storage_strategy = storage_strategy,
-        self.users = CompositeUserActions(storage_strategy)
+        self.users = CompositeUserActions(sm.get_storage_strategy())
         self.sheets_service = sheets_service
         self.messages = static_messages
-        self.message_manager = MessageManager(storage_strategy, sheets_service)
+        self.message_manager = MessageManager()
 
-    def get_daily(self, *args, **kwargs):
-        return get_daily_data_gsheets(self.sheets_service, *args, **kwargs)
+
+    def get_daiy_data_unit(self):
+        return self.message_manager.daily_builder.du
+
+    def get_daily_info(self):
+        ## functional
+        # return build_daily_info(self.sheets_service)
+        return self.message_manager.daily
+
+    def request_data(self, requested_data) -> str:
+        return self.message_manager.request_data(requested_data)
 
     def get_last_data(self, *args, **kwargs):
         return self.message_manager.get_daily_data_gsheets(*args, **kwargs)
-
-    def build_daily_info(self):
-        return build_daily_info(self.sheets_service)
-
-    def request_data(self, requested_dat) -> str:
-        #TODO
-        return ''
+    
--- app/instances.py
+++ app/instances.py
@@ -6,13 +6,17 @@ from storage.storage_manager import storage_manager
 # start firebase
 storage_manager.start_firebase()
 
-# last
-last_data = DataUnit().fill_data_from_gsheets()
 
 # viber bot Singleton init
 bot = Bot(storage_manager.get_storage_strategy(),
           storage_manager.get_sheets_service())
 
+
 viber = bot.viber
 
-bot.message_manager.populate_daily_data()
+last_data = bot.message_manager.daily_builder.du
+
+# # last
+# last_data = DataUnit()
+# last_data.fill_data_from_firebase()
+# bot.message_manager.populate_daily_data()
